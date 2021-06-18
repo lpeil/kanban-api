36f6649a5cab7a9981a81a67287da6bfe0385fd8
lpeil <luan@peil.dev>
2021-06-18 13:32:14 -0300

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
